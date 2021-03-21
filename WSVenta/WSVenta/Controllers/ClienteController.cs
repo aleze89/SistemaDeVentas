e29e2a208d3174a23c3f3c2e172eca054790f510
aleze89 <68794504+aleze89@users.noreply.github.com>
2021-03-21 02:33:24 -0300

    creacion y conexion base de datos

---
+++ WSVenta/WSVenta/Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSVenta.Models;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using(VentaDemoContext db= new VentaDemoContext()){
+                var lst = db.Clientes.ToList();
+                return Ok(lst);
+
+            }
+        }
+    }
+}
